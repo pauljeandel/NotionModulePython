63b0c4625ce5d94ff2448cd3cd873363cf4ac84e
Paul Jeandel <pauljeandel@hotmail.com>
2022-02-22 21:11:41 +0100

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,6 +1,6 @@
 import NotionModule
 
-instance = NotionModule.Notion('secret_xM6R0RWZX6Uykc3qidCGWeBLaUP1RtMytzqLuW8OvV8')
+instance = NotionModule.Notion('')
 
 
 page = instance.GetPage('29d13803fdd24b2bb4229bf6d19755cb')
